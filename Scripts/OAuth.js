6ad4c33ac4860525f34f0ed7ff45ef0fe058d2a4
RicardoBernardo870 <77888335+RicardoBernardo870@users.noreply.github.com>
2021-03-15 17:35:47 +0000

    Add files via upload

---
+++ Scripts/OAuth.js
@@ -0,0 +1,66 @@
+const signupForm = document.querySelector('#signup-form');
+const logout = document.querySelector('#logout');
+const login = document.querySelector('#login-form');
+const chat = document.querySelector('.chat-list');
+
+
+
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+      console.log(user)
+     setupUI(user);
+     chatroom.getChats(data => chatUI.render(data));
+     chat.innerHTML = ''
+       
+        
+    } else {
+      console.log('user is logged out')
+      setupUI();
+      chat.innerHTML = 'LOGIN TO SEE CHAT';
+      
+    
+        
+    }
+})
+
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = signupForm['signup-email'].value;
+    const pass = signupForm['signup-password'].value;
+
+
+
+    auth.createUserWithEmailAndPassword(email, pass).then(cred => {
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+});
+
+
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+
+login.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+
+    const email = login['login-email'].value;
+    const password = login['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        login.reset();
+    })
+    
+})
+
+
+
---
+++ Scripts/app.js
@@ -0,0 +1,73 @@
+// dom queries
+const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details');
+
+
+const setupUI = (user) => {
+    if (user) {
+        
+        const html = `<div>Logged in as ${user.email}`;
+        accountDetails.innerHTML = html;
+        loggedInLinks.forEach(item => item.style.display = 'Block');
+        loggedOutLinks.forEach(item => item.style.display = 'none');
+    } else {
+       
+        accountDetails.innerHTML = '';
+        loggedInLinks.forEach(item => item.style.display = 'none');
+        loggedOutLinks.forEach(item => item.style.display = 'block');
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+});
+
+
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+       .then(() => newChatForm.reset())
+       .catch(err => console.log(err));
+})
+
+
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+   
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    
+    newNameForm.reset();
+    
+    updateMssg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMssg.innerText = '', 3000);
+
+});
+
+
+rooms.addEventListener('click', e => {
+    if(e.target.tagName === 'BUTTON'){
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
+})
+
+const username = localStorage.username ? localStorage.username : 'anon';
+
+
+
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('general', username);
+
+
