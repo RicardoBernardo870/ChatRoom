7fe6f0b9a292ed05bb842c4f950becdefbb36499
RicardoBernardo870 <77888335+RicardoBernardo870@users.noreply.github.com>
2021-03-16 19:32:08 +0000

    Update app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -18,9 +18,7 @@ const setupUI = (user) => {
         loggedOutLinks.forEach(item => item.style.display = 'none');
     } else {
        
-        accountDetails.innerHTML = '';
-        loggedInLinks.forEach(item => item.style.display = 'none');
-        loggedOutLinks.forEach(item => item.style.display = 'block');
+    
     }
 }
 
